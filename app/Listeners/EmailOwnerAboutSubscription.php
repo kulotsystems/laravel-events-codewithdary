e1b75e61a27cd3318fec594bb33d29657d326c04
Arvic S. Babol <kulotsystems@gmail.com>
2022-09-13 13:45:08 +0800

    Continue listener.

--- app/Listeners/EmailOwnerAboutSubscription.php
+++ app/Listeners/EmailOwnerAboutSubscription.php
@@ -3,8 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\UserSubscribeEvent;
+use App\Models\NewsLetter;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
 
 class EmailOwnerAboutSubscription
 {
@@ -26,6 +28,10 @@ class EmailOwnerAboutSubscription
      */
     public function handle (UserSubscribeEvent $event)
     {
+        NewsLetter::create([
+            'email' => $event->email
+        ]);
 
+        Mail::to($event->email)->send();
     }
 }
--- app/Models/NewsLetter.php
+++ app/Models/NewsLetter.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class NewsLetter extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'email'
+    ];
 }
